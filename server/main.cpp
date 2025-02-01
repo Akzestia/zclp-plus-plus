91c2c534822453ffe7b72280d5f39c82ab63ffa3
アクゼスティア <akzestia@gmail.com>
2025-02-01 20:32:36 +0100

    zclp_tls integration and non-blocking async io for server, via using 2 different sockets for performing send/receive operations

--- server/main.cpp
+++ server/main.cpp
@@ -1,17 +1,12 @@
 #include <cstdio>
 
 #include "../tokio-cpp/tokio.hpp"
-#include "../zclp++.hpp"
 #include "server.h"
 
 int main(int argc, char* argv[]) {
-    // tokio::initialize();
-
-    Server server(8080);
-    printf("x");
+    zclp_tls::init();
+    Server server(6666, 6667);
     if (!server.run()) {
         printf("\nFailed to start server\n");
     }
-
-    // tokio::shutdown();
 };
--- server/server.h
+++ server/server.h
@@ -1,15 +1,24 @@
 #include <arpa/inet.h>
+
 #include <atomic>
 #include <cstdint>
 
+#include "../zclp_utils/zclp_utils.hpp"
+
 struct Server {
     [[nodiscard]] bool run();
-    [[nodiscard]] Server(uint16_t port) noexcept;
+    [[nodiscard]] Server(uint16_t listener_port, uint16_t sender_port) noexcept;
     void process_udp_pack(uint8_t* packet, ssize_t len);
-private:
-    int m_socket_fd;
-    struct sockaddr_in m_addr;
-    uint16_t m_port;
+    void send_ack_pack(sockaddr_in destiantion_addr) const;
+
+  private:
+    /*
+        using separate sockets for recv|send,
+        for non-blocking io
+    */
+    int m_listener_fd, m_sender_socket_fd;
+    struct sockaddr_in m_listener_addr, m_sender_addr;
+    uint16_t m_listener_port, m_sender_port;
     const int m_max_mtu;
 
     std::atomic<bool> m_is_running;
