b7a5c60c55a56d61db31aa155a35a7535d9bd6f5
アクゼスティア <akzestia@gmail.com>
2025-02-01 20:34:27 +0100

    zclp_tls integration

--- client/client.h
+++ client/client.h
@@ -3,17 +3,23 @@
 #include <atomic>
 #include <cstdint>
 
+#include "../zclp++.hpp"
+#include "../zclp_utils/zclp_utils.hpp"
+
 struct Client {
     [[nodiscard]] bool run();
     [[nodiscard]] Client(uint16_t port) noexcept;
     void process_udp_pack(uint8_t* packet, ssize_t len);
     [[nodiscard]] bool send(uint8_t* message, ssize_t len);
+    [[nodiscard]] bool connect();
+    [[nodiscard]] bool disconnect();
 
   private:
     int m_socket_fd;
     struct sockaddr_in m_addr;
     uint16_t m_port;
     const int m_max_mtu;
+    zclp_tls::zclp_tls_arena tls;
 
     std::atomic<bool> m_is_running;
 };
