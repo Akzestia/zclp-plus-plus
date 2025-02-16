b0fcb1dc587ce9f7e27299a05872bd1f1fe15729
アクゼスティア <akzestia@gmail.com>
2025-02-16 22:34:21 +0100

    Request Session, free_request() and create_request()

--- scylla/center_db.h
+++ scylla/center_db.h
@@ -1,5 +1,27 @@
+#include <cassandra.h>
 
+/*
+    Scylla db is only used on server side.
+*/
 
 namespace center_db {
 void init();
-}
+
+struct RequestSession {
+    CassSession* session;
+    CassCluster* cluster;
+    CassFuture* connect_future;
+    CassStatement* statement;
+    CassFuture* result_future;
+    const CassResult* result;
+    CassIterator* iterator;
+
+    [[nodiscard]] RequestSession() noexcept;
+    ~RequestSession() noexcept;
+};
+
+RequestSession create_request();
+
+void free_request(RequestSession& request);
+
+}  // namespace center_db
