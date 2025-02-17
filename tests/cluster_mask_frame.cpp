dc2e0303785fac837d4ad95453bb8da64220ef78
アクゼスティア <akzestia@gmail.com>
2025-02-17 13:38:52 +0100

    Reduced test count per unit test

--- tests/cluster_mask_frame.cpp
+++ tests/cluster_mask_frame.cpp
@@ -8,7 +8,7 @@ TEST(ClusterMaskFrameTest, EncodeDecode) {
     using namespace zclp_test_heplers;
     using namespace Frames;
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 100; i++) {
         ClusterMask _in(getSpecifiedDistribution(16, 32)), _out;
 
         uint8_t* out = new uint8_t[_in.byte_size()]();
