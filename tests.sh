ce0a86f9a6138db8253c32f4cb0b1845981249c1
アクゼスティア <akzestia@gmail.com>
2025-02-17 02:02:46 +0100

    Actions test

--- tests.sh
+++ tests.sh
@@ -5,7 +5,8 @@ IRed='\033[0;91m'
 IGreen='\033[0;92m'
 ColorReset='\033[0m'
 
-EXCLUDED_TESTS=("server_async")
+# header_protection
+EXCLUDED_TESTS=("server_async ")
 
 select_single_test=false
 select_multiple_tests=false
@@ -47,19 +48,30 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo -e "Options:"
     echo -e "  -s              Select a single test to compile"
     echo -e "  -x              Select multiple tests to compile"
+    echo -e "  --no-confirm    Automatically run the tests without confirmation"
     echo -e "  -h, --help      Display this help message"
     echo -e "${ICyan}Example:${ColorReset}"
     echo -e "  ./test.sh -s      # Select and compile a single test"
     echo -e "  ./test.sh -x      # Select and compile multiple tests"
     echo -e "  ./test.sh         # Compile all tests"
+    echo -e "  ./test.sh --no-confirm   # Automatically run the tests"
     exit 0
 fi
 
 test_files=($(grep "BUILD_.*_FRAME\|BUILD_.*_HEADER\|BUILD_.*_PROTECTION\|BUILD_.*_RESET\|BUILD_.*_NEGOTIATION\|BUILD_VL_INTEGER" ../CMakeLists.txt | sed 's/option(BUILD_\(.*\) "Build.*/\1/' | tr '[:upper:]' '[:lower:]'))
 
+EXCLUDED_TESTS=($(echo "${EXCLUDED_TESTS[@]}" | tr '[:upper:]' '[:lower:]'))
+
 for excluded in "${EXCLUDED_TESTS[@]}"; do
-    test_files=(${test_files[@]/$excluded})
+    for i in "${!test_files[@]}"; do
+        test_name_lower=$(echo "${test_files[$i]}" | tr '[:upper:]' '[:lower:]')
+        if [[ "$test_name_lower" == "$excluded" ]]; then
+            unset test_files[$i]
+        fi
+    done
 done
+
+test_files=("${test_files[@]}")
 
 if [[ "$1" == "-s" ]]; then
     select_single_test=true
@@ -102,25 +114,42 @@ else
 fi
 
 cd ../..
+if [[ "$1" == "--no-confirm" ]]; then
+    clear && clear
+    test_paths=()
+    for test in "${selected_tests[@]}"; do
+        test_paths+=("tests/bin/$test")
+    done
 
-echo ""
-read -r -p $'\e[0;96mBuild complete. Run Tests [Y/n]?\e[0m' user_input
+    res=$("./run_test.sh" "${test_paths[@]}");
 
-user_input=${user_input,,}
-if [[ "$user_input" =~ ^(no|n)$ ]]; then
-    exit 0
-elif [[ "$user_input" =~ ^(yes|y|)$ ]]; then
-    if [[ ${#selected_tests[@]} -gt 0 ]]; then
-        clear && clear
-        test_paths=()
-        for test in "${selected_tests[@]}"; do
-            test_paths+=("tests/bin/$test")
-        done
-        exec "./run_test.sh" "${test_paths[@]}"
+    if echo "$res" | grep "FAILED"; then
+        echo -e "${IRed}Tests failed.${ColorReset}"
+        exit 1
     else
-        clear && clear
-        exec "./run_test.sh"
+        echo -e "${IGreen}Tests passed.${ColorReset}"
     fi
+
 else
-    exit 0
+    echo ""
+    read -r -p $'\e[0;96mBuild complete. Run Tests [Y/n]?\e[0m' user_input
+
+    user_input=${user_input,,}
+    if [[ "$user_input" =~ ^(no|n)$ ]]; then
+        exit 0
+    elif [[ "$user_input" =~ ^(yes|y|)$ ]]; then
+        if [[ ${#selected_tests[@]} -gt 0 ]]; then
+            clear && clear
+            test_paths=()
+            for test in "${selected_tests[@]}"; do
+                test_paths+=("tests/bin/$test")
+            done
+            "./run_test.sh" "${test_paths[@]}";
+        else
+            clear && clear
+            exec "./run_test.sh"
+        fi
+    else
+        exit 0
+    fi
 fi
