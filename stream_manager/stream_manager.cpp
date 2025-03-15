bec6e4f6aa9aff2af7927dd9196546d7b28d9449
アクゼスティア <akzestia@gmail.com>
2025-03-15 22:08:49 +0100

    Stream manager

---
+++ stream_manager/stream_manager.cpp
@@ -0,0 +1,17 @@
+#include "stream_manager.h"
+
+namespace stream_manager {
+
+void init() {
+}
+
+void create_stream() {
+}
+
+void remove_stream() {
+}
+
+void clear() {
+}
+
+}  // namespace stream_manager
---
+++ stream_manager/stream_manager.h
@@ -0,0 +1,15 @@
+#ifndef STREAM_MANAGER_H
+#define STREAM_MANAGER_H
+
+namespace stream_manager {
+
+void init();
+
+void create_stream();
+void remove_stream();
+
+void clear();
+
+}  // namespace stream_manager
+
+#endif  // STREAM_MANAGER_H
