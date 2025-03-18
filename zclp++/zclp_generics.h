cc2e3cb0dfe892504ebacd74cc5ea7f4b7a034fc
アクゼスティア <akzestia@gmail.com>
2025-03-18 23:49:08 +0100

    Zclp Generics .h

---
+++ zclp++/zclp_generics.h
@@ -0,0 +1,32 @@
+#ifndef ZCLP_GENERICS_H
+#define ZCLP_GENERICS_H
+
+#include <variant>
+
+#include "../client/client_errors.h"
+#include "../server/server_errors.h"
+
+namespace zclp_generics {
+
+enum class GenericResults : uint8_t {
+    Success = 0,
+    Failure = 1,
+    Undefined = 2,
+};
+
+using ZclpError = std::variant<client_errors::ClientError,
+                               server_errors::ServerError, GenericResults>;
+
+struct ZclpResult {
+    bool success = false;
+    ZclpError error;
+
+    bool operator!();
+
+    [[nodiscard]] static ZclpResult Success() noexcept;
+    [[nodiscard]] static ZclpResult Failure(ZclpError error) noexcept;
+};
+
+}  // namespace zclp_generics
+
+#endif  // ZCLP_GENERICS_H
